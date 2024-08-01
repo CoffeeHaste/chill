a33b3df6176d743c0bd77cb0442462f2a4ab94e6
CoffeeHaste <eryeyrfhgdf@gmail.com>
2024-08-02 00:27:34 +0700

    Update meow.sh

--- meow.sh
+++ meow.sh
@@ -1,3 +1,4 @@
+apt-get install ssh
 export root_password="kD82x6KYPgvnWq9zs7XTGcPs9RFAvbfYdaZM6tr5LHm2"
 export user_password="FB96qDyY7VfzW5henJP3upVfT3bwyB5UMsJCt6KQahPH"
 export user_name="paimon"
