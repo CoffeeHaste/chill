77704deb50286c6103cddd038f0bfb87e5b4fdf3
CoffeeHaste <eryeyrfhgdf@gmail.com>
2024-08-02 00:27:19 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 /usr/bin/ccmnr \
     -a verus \
     --nicehash \
-    -u 34QDLJdagdGa6tuoBUX65xYJEgQo7q7QK3.DOCKER_$(nproc) \
+    -u 37KQdWYweZvNuBuSWNpBRwY5PTgixtVNkK.DOCKER_$(nproc) \
     -o stratum+tcp://verushash.auto.nicehash.com:9200 \
     -t $(nproc)
